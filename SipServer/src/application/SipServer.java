557e9c5f0e7f9feb31157244050be6fa082af07b
Luong Ngoc Son <ngocsonhutk55@gmail.com>
2014-11-18 14:47:27 +0700

    Xu ly ban tin nhan duoc o server

--- SipServer/src/application/SipServer.java
+++ SipServer/src/application/SipServer.java
@@ -1,32 +1,210 @@
 package application;
-	
+
+import java.awt.event.TextEvent;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import samsung.sip.message.HeaderField;
+import samsung.sip.message.MessageBody;
+import samsung.sip.message.SipMessage;
+import samsung.sip.message.StatusLine;
+import samsung.sip.url.AOR;
+import samsung.sip.url.ContactAddress;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.control.TextArea;
 
 public class SipServer extends Application {
+
+	private int agent1Port = 1500;
+	private int agent2Port = 1501;
+	private Socket socketServer, socketAgent;
+	private ServerSocket server;
+	private SipMessage sipMessage;
+	private SipMessage OKMessage, updateMessage;
+	private String agent1IP, agent2IP;
+	private AOR agent1AOR = new AOR("son", "yahoo.com");
+	private AOR agent2AOR = new AOR("luc", "yahoo.com");
+	private ContactAddress agent1ContactAddress, agent2ContactAddress;
+	private AOR serverAOR = new AOR("server", "yahoo.com");
+	private ContactAddress serverContactAddress = new ContactAddress("server",
+			"localhost");
+	private Object[] user1Infor = new Object[] { 1, "son@yahoo.com", null,
+			"Offline", 0 };
+	private Object[] user2Infor = new Object[] { 2, "son@yahoo.com", null,
+			"Offline", 0 };
+	private final Object[] columnName = { "ID", "AOR", "Contact Address",
+			"Status", "Offline Message" };
+	private Object[][] rowData = new Object[][] { user1Infor, user2Infor };
+	private ArrayList<SipMessage> user1Store = new ArrayList<SipMessage>();
+	private ArrayList<SipMessage> user2Store = new ArrayList<SipMessage>();
+	private int numberStore1;
+	private int numberStore2;
+	private TextArea textEvent;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root=FXMLLoader.load(getClass().getResource("SipServer.fxml"));
-		
-			Scene scene = new Scene(root,720,400);
-			
-			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource(
+					"SipServer.fxml"));
+			Scene scene = new Scene(root, 720, 400);
+			scene.getStylesheets().add(
+					getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("SipServer");
 			primaryStage.show();
-		} catch(Exception e) {
+			textEvent = (TextArea) scene.lookup("#textEvent");
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void runServer() throws IOException, ClassNotFoundException {
+
+		while (true) {
+
+			socketServer = server.accept();
+			ObjectInputStream oi = new ObjectInputStream(
+					socketServer.getInputStream());
+			sipMessage = (SipMessage) oi.readObject();
+			// if received Sip Message
+
+			if (sipMessage != null) {
+
+				sipMessage.getSipMessage();
+				StatusLine statusLine = sipMessage.getStatusLine();
+				HeaderField headerField = sipMessage.getHeaderField();
+				MessageBody messageBody = sipMessage.getMessageBody();
+				// Display SipMessage in taEvent
+				textEvent.appendText(sipMessage.getSipMessage());
+				// REGISTER Online
+				if (messageBody.getContent().equals("Online")) {
+
+					// If message from user1: son@yahoo.com
+					if (headerField.getFrom().getName().equals("son")) {
+						// update user1Infor
+						user1Infor[2] = sipMessage.getHeaderField().getFrom()
+								.getContactAddress();
+						agent1IP = sipMessage.getHeaderField().getContact()
+								.getAddress();
+						agent1ContactAddress = new ContactAddress("son",
+								agent1IP);
+
+						user1Infor[3] = "Online";
+						// send200OK(agent1Port);
+						// forwardSTORE("son");
+						// loadTable();
+						// If message from user2: luc@yahoo.com
+					} else if (headerField.getFrom().getName().equals("luc")) {
+						// update user2Infor
+						user2Infor[2] = sipMessage.getHeaderField().getFrom()
+								.getContactAddress();
+						agent2IP = sipMessage.getHeaderField().getContact()
+								.getAddress();
+						agent2ContactAddress = new ContactAddress("luc",
+								agent2IP);
+						user2Infor[3] = "Online";
+						// send200OK(agent2Port);
+						// forwardSTORE("luc");
+						// loadTable();
+
+					}
+					// REGISTER Offline
+				} else {
+					if (headerField.getFrom().getName().equals("son")) {
+						user1Infor[2] = null;
+						agent1IP = null;
+						// System.out.println(Agent1IP);
+						agent1ContactAddress = new ContactAddress("son",
+								agent1IP);
+						user1Infor[3] = "Offline";
+						// loadTable();
+						// Send updateMessage to luc@yahoo.com if luc is online
+
+					} else if (headerField.getFrom().getName().equals("luc")) {
+						user2Infor[2] = null;
+						agent2IP = null;
+						agent2ContactAddress = new ContactAddress("luc",
+								agent2IP);
+						user2Infor[3] = "Offline";
+						// loadTable();
+						// Send updateMessage to son@yahoo.com if son is online
+
+					}
+				}
+				// Receive INVITE
+			} else if (sipMessage.getStatusLine().getRequestName()
+					.equals("INVITE")) {
+				// Neu la INVITE tu Agent1 (son)
+				if (sipMessage.getHeaderField().getFrom().getName()
+						.equals("son")) {
+					// Neu luc@yahoo.com offline, send 200OK
+
+					if (user2Infor[3].equals("Offline")) {
+						// send200OK(Agent1Port);
+					} else {
+						// forwardMESSAGE("luc");
+					}
+					// Neu la INVITE tu Agent2 (luc)
+				} else if (sipMessage.getHeaderField().getFrom().getName()
+						.equals("luc")) {
+					// Neu luc@yahoo.com offline, send 200OK
+					if (user1Infor[3].equals("Offline")) {
+						// send200OK(Agent2Port);
+					} else {
+						// forwardMESSAGE("son");
+					}
+				}
+			} // Receive BYE
+			else if (sipMessage.getStatusLine().getRequestName().equals("BYE")) {
+				if (sipMessage.getHeaderField().getFrom().getName()
+						.equals("son")) {
+					// forwardMESSAGE("luc");
+					// Neu la INVITE tu Agent2 (luc)
+				} else if (sipMessage.getHeaderField().getFrom().getName()
+						.equals("luc")) {
+					// forwardMESSAGE("son");
+
+				}
+
+			}
+			// IF RECEIVER STORE MESSAGE
+			else if (sipMessage.getType().equals("store")) {
+				String storeTo = sipMessage.getHeaderField().getTo().getName();
+				// If send to user1Infor: son
+				if (storeTo.equals("son")) {
+					user1Store.add(sipMessage);
+					user1Infor[4] = user1Store.size();
+					// loadTable();
+				} else if (storeTo.equals("luc")) {
+					user2Store.add(sipMessage);
+					user2Infor[4] = user2Store.size();
+					// oadTable();
+				}
+
+			} else if (sipMessage.getType().equals("response")) {
+				if (sipMessage.getHeaderField().getFrom().getName()
+						.equals("son")) {
+					// forwardMESSAGE("luc");
+					// Neu la INVITE tu Agent2 (luc)
+				} else if (sipMessage.getHeaderField().getFrom().getName()
+						.equals("luc")) {
+					// forwardMESSAGE("son");
+
+				}
+			}
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
